591e6ea58b094e598355bbfc409defd6eeb24758
LeBac99 <pakcop22799@gmail.com>
2020-03-10 16:58:26 +0700

    update login 1

--- app/User.php
+++ app/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','phone_number','gender','address','name_shop','store_address','role'
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Route as SymfonyRoute;
 // client
 Route::get('/', function () {
     return view('client.index');
@@ -17,7 +20,15 @@ Route::get('/', function () {
 
 // end client
 // admmin
-Route::get('admin1',function(){
-    return view('admin.hompage');
+Route::group(['middleware' => 'auth'], function () {
+    Route::group(['middleware' => 'mod.check'], function () {
+        Route::get('admin1', function () {
+            return view('admin.hompage');
+        });
+    });
 });
 // end
+// login
+Route::get('cp-login', 'Auth\LoginController@loginForm');
+Route::post('cp-login', 'Auth\LoginController@postLogin')->name('login');
+Route::any('logout', 'Auth\LoginController@logout')->name('logout');
